24cfb3d951d61291ad0865e43c8993a5dfaebb4c
Toan Tran <barneytran1998@gmail.com>
2020-10-13 16:24:29 -0500

    Python Coding Standard TODO: Merge

--- test.py
+++ test.py
@@ -1,14 +1,13 @@
 from WiSun import WiSun
 
-
 if __name__ == "__main__":
     WiSun = WiSun("./devices.json")
 
-    print("\nRunning \"config_border_router_gtks_put\" on all Devices on PC1: 192.168.1.53")
+    print('\nRunning "config_border_router_gtks_put" on all Devices on PC1: 192.168.1.53')
 
     for i in range(1,5):
         print(WiSun.config_border_router_gtks_put(PC=1, device=i))
 
-    print("\nRunning \"config_border_router_gtks_put\" on all Devices on PC1: 192.168.1.53")
+    print('\nRunning "config_border_router_gtks_put" on all Devices on PC1: 192.168.1.53')
     for i in range(1, 5):
         print(WiSun.config_border_router_gtks_put(PC=2, device=i))
